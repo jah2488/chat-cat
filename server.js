3112c05e91f92d837b555a5b4d242a83db7a2458
Justin Herrick <justin@justinherrick.com>
2014-10-22 10:06:44 -0400

    Removing logger.

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 var express = require("express");
-var logfmt = require("logfmt");
 var app = express();
 
-app.use(logfmt.requestLogger());
-
 app.use(express.static(__dirname + '/dist'));
-
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
